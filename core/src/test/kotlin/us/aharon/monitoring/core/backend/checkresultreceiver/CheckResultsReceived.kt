2c6fb34253a42657b9d5262c969425c7f1d3db88
Dan Aharon <dan@danielaharon.com>
2018-10-06 19:28:58 +0300

    Testing

--- core/src/test/kotlin/us/aharon/monitoring/core/backend/checkresultreceiver/CheckResultsReceived.kt
+++ core/src/test/kotlin/us/aharon/monitoring/core/backend/checkresultreceiver/CheckResultsReceived.kt
@@ -2,17 +2,15 @@
  * Copyright (c) 2018 Daniel Aharon
  */
 
-package us.aharon.monitoring.core.backend.checkResultStatus
+package us.aharon.monitoring.core.backend.checkresultreceiver
 
 import cloud.localstack.LocalstackExtension
-import com.amazonaws.regions.Regions
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput
-import com.amazonaws.services.lambda.runtime.events.SQSEvent
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -21,6 +19,7 @@ import org.koin.standalone.inject
 
 import us.aharon.monitoring.core.BaseTest
 import us.aharon.monitoring.core.backend.CheckResultReceiver
+import us.aharon.monitoring.core.common.SQSTestEvent
 import us.aharon.monitoring.core.db.CheckResultRecord
 import us.aharon.monitoring.core.db.CheckResultStatus
 
@@ -29,35 +28,37 @@ import kotlin.test.assertEquals
 
 
 @ExtendWith(LocalstackExtension::class)
-class SingleCheckResultStatus : BaseTest() {
+class CheckResultsReceived : BaseTest() {
 
-    private val json: ObjectMapper by inject()
     private val db: DynamoDBMapper by inject()
-    private val testEvent = SQSEvent().apply {
-        records = listOf(
-                SQSEvent.SQSMessage().apply {
-                    messageId = "19dd0b57-b21e-4ac1-bd88-01bbb068cb78"
-                    receiptHandle = "MessageReceiptHandle"
-                    attributes = mapOf(
-                            "ApproximateReceiveCount" to "1",
-                            "SentTimestamp" to "1523232000000",
-                            "SenderId" to "123456789012",
-                            "ApproximateFirstReceiveTimestamp" to "1523232000001"
-                    )
-                    messageAttributes = emptyMap()
-                    md5OfBody = "7b270e59b47ff90a553787216d55d91d"
-                    eventSource = "aws:sqs"
-                    eventSourceArn = "arn:aws:sqs:us-east-1:123456789012:CheckResultsQueue"
-                    awsRegion = Regions.US_EAST_1.name
-                    body = json.writeValueAsString(mapOf<String, String>(
-                            "name" to "server-1.example.com",
-                            "timestamp" to "2018-08-23T11:39:44Z",
-                            "status" to CheckResultStatus.OK.name,
-                            "output" to "OK: This check is A-OK"
-                    ))
-                }
-        )
-    }
+    private val singleTestEvent = SQSTestEvent(listOf(
+            mapOf(
+                    "name" to "server-1.example.com",
+                    "timestamp" to "2018-08-23T11:39:44Z",
+                    "status" to CheckResultStatus.OK.name,
+                    "output" to "OK: This check is A-OK"
+            )
+    ))
+    private val multipleTestEvents = SQSTestEvent(listOf(
+            mapOf(
+                    "name" to "server-1.example.com",
+                    "timestamp" to "2018-08-23T11:39:44Z",
+                    "status" to CheckResultStatus.OK.name,
+                    "output" to "OK: This check is A-OK."
+            ),
+            mapOf(
+                    "name" to "server-3.example.com",
+                    "timestamp" to "2018-08-23T11:43:00Z",
+                    "status" to CheckResultStatus.WARNING.name,
+                    "output" to "WARNING: We're heading for a bad time."
+            ),
+            mapOf(
+                    "name" to "server-2.example.com",
+                    "timestamp" to "2018-08-23T11:41:00Z",
+                    "status" to CheckResultStatus.CRITICAL.name,
+                    "output" to "CRITICAL: This check failed."
+            )
+    ))
 
 
     @BeforeEach
@@ -76,9 +77,9 @@ class SingleCheckResultStatus : BaseTest() {
     }
 
     @Test
-    fun `Process a single check result`() {
+    fun `Receive a single check result`() {
         // Run the Lambda handler.
-        CheckResultReceiver().run(testEvent)
+        CheckResultReceiver().run(singleTestEvent)
 
         // Query the database to verify that the expected data is saved.
         val query = DynamoDBQueryExpression<CheckResultRecord>()
@@ -100,5 +101,17 @@ class SingleCheckResultStatus : BaseTest() {
             assertEquals(CheckResultStatus.OK, this.status)
             assertEquals("OK: This check is A-OK", this.output)
         }
+    }
+
+    @Test
+    fun `Receive multiple check results`() {
+        // Run the Lambda handler.
+        CheckResultReceiver().run(multipleTestEvents)
+
+        // Scan the database to verify that the events were saved.
+        val scan = DynamoDBScanExpression()
+        val results = db.scan(CheckResultRecord::class.java, scan)
+
+        assertEquals(3, results.size)
     }
 }
--- core/src/test/kotlin/us/aharon/monitoring/core/backend/clientcleanup/RemoveTest.kt
+++ core/src/test/kotlin/us/aharon/monitoring/core/backend/clientcleanup/RemoveTest.kt
@@ -10,6 +10,8 @@ import com.amazonaws.services.dynamodbv2.model.OperationType
 import com.amazonaws.services.dynamodbv2.model.StreamRecord
 import com.amazonaws.services.sns.AmazonSNS
 import com.amazonaws.services.sqs.AmazonSQS
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -23,7 +25,7 @@ import us.aharon.monitoring.core.common.FAKE_SQS_QUEUE_ARN
 import us.aharon.monitoring.core.common.FAKE_SQS_QUEUE_URL
 
 
-// TODO: Broken test.  Sending to LocalStack SNS topic does not work.
+// TODO:  Test is broken.  SNS & SQS with LocalStack not working correctly?
 @Disabled
 @ExtendWith(LocalstackExtension::class)
 class RemoveTest : BaseTest() {
@@ -42,6 +44,18 @@ class RemoveTest : BaseTest() {
             ) to OperationType.REMOVE
     ))
 
+
+    @BeforeEach
+    fun createSNSTopic() {
+        sns.createTopic("CLIENT_CHECK_TOPIC")
+        sns.createTopic("SERVERLESS_CHECK_TOPIC")
+    }
+
+    @AfterEach
+    fun deleteSNSTopic() {
+        sns.deleteTopic("CLIENT_CHECK_TOPIC")
+        sns.deleteTopic("SERVERLESS_CHECK_TOPIC")
+    }
 
     @Test
     fun `DynamoDB event REMOVE`() {
